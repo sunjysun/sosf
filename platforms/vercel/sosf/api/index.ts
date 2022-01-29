67059f53c768ab4ff32db4d3e878d08abe3aab85
sunjysun <sunjysun@126.com>
2022-01-29 14:28:46 +0800

    Fix error when visit a folder

--- platforms/vercel/sosf/api/index.ts
+++ platforms/vercel/sosf/api/index.ts
@@ -99,7 +99,7 @@ export default async function handler({
     }
     // Render file
     const data = await getItem(path, access_token, id)
-    if (data) {
+    if (data['@microsoft.graph.downloadUrl']) {
       return {
         statusCode: 308,
         headers: { Location: data['@microsoft.graph.downloadUrl'].slice(6) },
